4a2c418c7f213511ec2b1fa0040ff81a82425b68
maucerit <maucerit@ufl.edu>
2025-03-28 17:41:19 +0000

    updates copilit intro and created fastmpi scripts

--- main.py
+++ main.py
@@ -9,3 +9,24 @@ try:
     print(divide(10, 0))
 except ValueError as e:
     print(e)
+
+import importlib
+import my_module
+importlib.reload(my_module.math_operations)
+importlib.reload(my_module)
+from my_module import add, subtract, multiply, divide# Step 9: Using `sys.path` to Add Module Directories
+
+
+import sys
+import os
+import importlib
+module_path='/workspaces/BME3053C-Spring-2025'
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+import my_module
+importlib.reload(my_module.math_operations)
+importlib.reload(my_module)
+from my_module import add, subtract, multiply, divide
+
+print(f"5 + 3 = {add(5, 3)}")
